e296fedda822e8b9c65e0955d57fbf091d650b40
Francesc Sastre Cabot <francesc.sastrecabot@showpad.com>
2021-11-21 13:41:53 +0100

    add await

--- src/pdf/start-step-function.ts
+++ src/pdf/start-step-function.ts
@@ -5,16 +5,15 @@ import {
 } from "@aws-sdk/client-sfn";
 
 export const handler = async (event: any = {}): Promise<any> => {
-  console.log("start-exec");
-  console.log(event);
   const stepFunctionArn: string = process.env.stepFunctionArn!!;
   const client = new SFNClient({});
-  event.Records.map(async (entry: any) => {
-    const params: StartExecutionCommandInput = {
-      stateMachineArn: stepFunctionArn,
-      input: entry.body,
-    };
-    console.log(params);
-    await client.send(new StartExecutionCommand(params));
-  });
+  await Promise.all(
+    event.Records.map(async (entry: any) => {
+      const params: StartExecutionCommandInput = {
+        stateMachineArn: stepFunctionArn,
+        input: entry.body,
+      };
+      await client.send(new StartExecutionCommand(params));
+    })
+  );
 };
